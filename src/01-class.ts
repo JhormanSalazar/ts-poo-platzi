6177d2d814fbc45ca96825bf2a8acc4419e2a942
Jhorman Salazar <salazarjhorman181@gmail.com>
2024-12-26 23:10:52 -0500

    add standar config

---
+++ src/01-class.ts
@@ -0,0 +1,13 @@
+class myDate {
+    year: number;
+    month: number; 
+    day: number;
+
+    constructor(year: number, month: number, day: number){
+        this.year = year; 
+        this.month = month;
+        this.day = day;
+    }
+}
+
+const myDate1 = new myDate(2024, 11, 26); // 2024, diciembre, 26.
